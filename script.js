c371d104a23df07795d174bd95429876788ba831
wesley <wesley>
2022-11-10 18:04:31 -0300

    Operadores

---
+++ script.js
@@ -0,0 +1,31 @@
+var idade = 19;
+
+console.log(idade);
+
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var testeNaN = '100s' / '2';
+
+console.log(testeNaN);
+
+// Somar a string '200' com o número 50 e retornar 250
+var str = +'200' + 50;
+
+console.log(str);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+  
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
